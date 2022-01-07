1f283a42bed6b4ab346214491051ffd49848a4fb
Kevin S <kevin@chilicode.ca>
2022-01-06 18:00:29 -0800

    [NEW] VASTLY updated sorting algorithms to provide WAY better matching

---
+++ manual-sort.js
@@ -0,0 +1,26 @@
+const Path = require('path');
+const fs = require('fs-extra');
+const os = require('os');
+const Filter = require('./lib/media-filter');
+
+const config = require('./config.json');
+
+(async () => {
+  const finalConfig = Object.assign({}, config, {
+    "output": '--',
+    "movies": process.argv[4],
+    "tv": process.argv[3]
+  })
+
+  const filter = new Filter(finalConfig);
+
+  const files = fs.readdirSync(process.argv[2]);
+  for(const file of files) {
+    const src = Path.join(process.argv[2], file);
+    const result = await filter.getOutputFile(src);
+    if(result && !/--/.test(result)) {
+      console.log(result);
+      // await fs.move(src, result, { overwrite: true });
+    }
+  }
+})();
